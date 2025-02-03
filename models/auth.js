acb763115a6a4352286ec712186768974e1e87c0
Raikhanov <145896630+Raikhanov@users.noreply.github.com>
2025-02-03 18:01:33 +0500

    Update auth.js

--- models/auth.js
+++ models/auth.js
@@ -6,7 +6,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: process.env.CALLBACK_URL || "http://localhost:3000/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
